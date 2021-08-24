a5ea9811bcf675fdc17e3b7facd71ada84af2a75
Harsh R Jain <rjainharsh@gmail.com>
2021-08-24 08:46:40 +0530

    ADDED: method that aloows user to place a bet

--- src/com/bridgelabz/gamblingSimulator/GamblingSimulator.java
+++ src/com/bridgelabz/gamblingSimulator/GamblingSimulator.java
@@ -1,11 +1,25 @@
 package com.bridgelabz.gamblingSimulator;
 
 public class GamblingSimulator {
-
+	
+	public static final int STARTING_STAKE = 100;
+	public static final int EVERY_GAME_BET = 1;
+	
+	//uc2 - As a Gambler make $1 bet so either win or loose $1
+	public static void makeBet() {
+		
+		if(Math.random() > 0.5) {
+			System.out.println("Won $"+EVERY_GAME_BET);
+		}
+		else {
+			System.out.println("Lost $"+EVERY_GAME_BET);
+		}
+		
+	}
+	
 	public static void main(String[] args) {
 		
-		int STARTING_STAKE = 100;
-		int EVERY_GAME_BET = 1;
+		makeBet();
 		
 	}
 
