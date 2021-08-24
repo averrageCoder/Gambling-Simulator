37067414ad1adc47c53e73180299115e51692e84
Harsh R Jain <rjainharsh@gmail.com>
2021-08-24 08:21:52 +0530

    ADDED: uc1 with default initializations

---
+++ src/com/bridgelabz/gamblingSimulator/GamblingSimulator.java
@@ -0,0 +1,12 @@
+package com.bridgelabz.gamblingSimulator;
+
+public class GamblingSimulator {
+
+	public static void main(String[] args) {
+		
+		int STARTING_STAKE = 100;
+		int EVERY_GAME_BET = 1;
+		
+	}
+
+}
