f567cdfcea94e42efdc07f10ac6e53f72b20b0ac
Harsh R Jain <rjainharsh@gmail.com>
2021-08-24 09:47:36 +0530

    ADDED: calculate total amount after 20 days

--- src/com/bridgelabz/gamblingSimulator/GamblingSimulator.java
+++ src/com/bridgelabz/gamblingSimulator/GamblingSimulator.java
@@ -1,9 +1,12 @@
 package com.bridgelabz.gamblingSimulator;
 
+import java.util.Iterator;
+
 public class GamblingSimulator {
 	
 	public static final int STARTING_STAKE = 100;
 	public static final int EVERY_GAME_BET = 1;
+	public static final int DAYS_PLAYED = 20;
 	public static int currentStake;
 	
 	//uc2 - As a Gambler make $1 bet so either win or loose $1
@@ -11,33 +14,44 @@ public class GamblingSimulator {
 		
 		if(Math.random() > 0.5) {
 			currentStake+=EVERY_GAME_BET;
-			System.out.println("Won $"+EVERY_GAME_BET);
+			//System.out.println("Won $"+EVERY_GAME_BET);
 		}
 		else {
 			currentStake-=EVERY_GAME_BET;
-			System.out.println("Lost $"+EVERY_GAME_BET);
+			//System.out.println("Lost $"+EVERY_GAME_BET);
 		}
 		
 	}
 	
 	//uc3 As a Calculative Gambler if won or lost 50% of the stake, would resign for the day
-	public static Boolean checkResignGame() {
-		
-		if(currentStake >= STARTING_STAKE*1.5 || currentStake <= STARTING_STAKE*0.5) {
-			return true;
-		}
-		else
-			return false;
-	}
-	
-	public static void main(String[] args) {
-		currentStake = STARTING_STAKE;
+	public static void checkResignGame() {
 		Boolean shouldResign=false;
 		while(!shouldResign) {
 			makeBet();
-			shouldResign=checkResignGame();
+			if(currentStake >= STARTING_STAKE*1.5 || currentStake <= STARTING_STAKE*0.5) {
+				shouldResign= true;
+			}
+			else
+				shouldResign= false;
 		}
-		System.out.println("Resigning game with $"+currentStake);
+		
+	}
+	
+	//uc4 - After 20 days of playing every day would like to know the total amount won or lost.
+	public static void playFor20Days() {
+		int totalWinnings = 0;
+		for (int i = 1; i <= DAYS_PLAYED; i++) {
+			currentStake = STARTING_STAKE;
+			checkResignGame();
+			totalWinnings+=currentStake;
+			System.out.println("Resigning Day "+i+" with $"+currentStake);
+		}
+		System.out.println("Total winnings after "+DAYS_PLAYED+" days: "+totalWinnings);
+	}
+	public static void main(String[] args) {
+		
+		playFor20Days();
+		
 	}
 
 }
